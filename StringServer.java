0d81799d2f8e1c1fe976c3b32c8743db76d346b7
Rutracrafter <artur.melo.rodrigues@gmail.com>
2023-01-29 20:53:14 -0800

    created the server that concatenates text input via query to the webpage.

---
+++ StringServer.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    // int num = 0;
+    ArrayList<String> stringList = new ArrayList<>();
+
+    String message = "Use /add-message?s=<string> to concatenate text to this page!";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return message;
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    message += "\n" + parameters[1];
+                    return message;
+                } 
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
